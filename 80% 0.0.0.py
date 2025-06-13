aededa67bdca490a1d5f70d658da4752d68eaaa8
LGT1999 <liz.godoy@duocuc.cl>
2025-06-12 20:48:56 -0400

    Add files via upload

---
+++ 80% 0.0.0.py
@@ -0,0 +1,18 @@
+#Realice un programa el cual va a permitir generar un menú de ingreso de usuarios 
+
+
+
+
+
+
+def ingresar_usuarios():
+    
+    nombreUsuario = str(input("ingrese nombre de usuario: "))
+    sexo = str(input("ingrese Sexo (M o F): "))
+    contrasena = str(input("Ingrese Contraseña (mínimo 8 caracteres): "))
+
+def buscar_usuarios():
+
+def eliminar_usuario():
+
+def salir():
